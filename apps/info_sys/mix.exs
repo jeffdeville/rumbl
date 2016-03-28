cdc272d63e45e14e3705ad0dca62a779288ee8b8
Jeff Deville <jeffdeville@gmail.com>
2016-03-28 12:51:34 -0400

    Trying to get infosys to build

--- apps/info_sys/mix.exs
+++ apps/info_sys/mix.exs
@@ -9,6 +9,7 @@ defmodule InfoSys.Mixfile do
      deps_path: "../../deps",
      lockfile: "../../mix.lock",
      elixir: "~> 1.2",
+     elixirc_paths: elixirc_paths(Mix.env),
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
      deps: deps]
@@ -18,10 +19,12 @@ defmodule InfoSys.Mixfile do
   #
   # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger],
-     mod: {InfoSys, []}]
+    [mod: {InfoSys, []},
+     applications: [:logger]]
   end
 
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_),     do: ["lib"]
   # Dependencies can be Hex packages:
   #
   #   {:mydep, "~> 0.3.0"}
